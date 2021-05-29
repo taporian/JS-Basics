aa9cbce109cfb26fb6fcfbeada24d30fc961b4d3
taporian <christapor.har@gmail.com>
2021-05-29 21:37:34 +0300

    JS Basics 02 Step 1

---
+++ javascript-basics-02/step 1/index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.querySelector(".image")
+  }
+  function changePic1() {
+    document.getElementById("image1").src = "images/image1_2.jpg";
+  }
+  function changePic2(){
+    document.getElementById("image1").src = "images/image1.jpg";
+  }
+// let img = document.querySelector(".image1");
+// let start = img.src="images/image1.jpg";
+// let hover = img.src="images/image1_2.jpg";
+// img.onmouseover= function () {
+//     img.src = hover;
+// }
+// img.onmouseout = function() {
+// img.src = start;
+// }  // or put script under the image for this to work
+
