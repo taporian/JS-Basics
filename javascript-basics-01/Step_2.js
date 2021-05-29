51854ee931b5162a5465a7ef7d54719f1dba8aff
taporian <christapor.har@gmail.com>
2021-05-29 19:09:40 +0300

    Step_2

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,8 @@
+var nom ="Doe";
+var prenom="John";
+var Ville= "New York";
+var alert;
+function printbox(){
+    var myText=("Nom : "+nom+"\n"+"Prenom : "+prenom+"\n"+"Ville : "+Ville);
+    alert(myText);
+}
