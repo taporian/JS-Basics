0b277fc368fd02e81945666866e6ae87d88c01e4
taporian <christapor.har@gmail.com>
2021-05-29 21:51:39 +0300

    JS Basics 02 Step 2

--- javascript-basics-02/step 2/index.js
+++ javascript-basics-02/step 2/index.js
@@ -1,4 +1,10 @@
-function myFunction (){
-    var x =document.getElementById("name");
+
+let x = document.querySelector('#name');
+x.onblur= function(){
     alert("thank you for participating!");
 }
+
+// function myFunction (){
+//     var x =document.getElementById("name");
+//     alert("thank you for participating!");
+// }
