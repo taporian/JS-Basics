dd992557edfce8744acfb61ce029e65ce03725e7
taporian <christapor.har@gmail.com>
2021-05-29 20:29:33 +0300

    Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,15 @@
+function userInput(){
+    document.getElementById("user").submit();
+    var first_number = parseInt(document.getElementById("first_number").value);
+    var second_number = parseInt(document.getElementById("second_number").value);
+   
+    var sum = second_number +first_number;
+    if(isNaN(sum)){
+        alert("You can only insert numbers");
+    }
+    else{
+        alert("The Sum = "+sum);
+    }
+   
+
+}
