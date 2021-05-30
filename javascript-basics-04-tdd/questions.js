460e04d063f0335bfc83f210d489ace0d9fe1378
taporian <christapor.har@gmail.com>
2021-05-30 06:09:42 +0300

    JS Basics 04 Done

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,101 +1,130 @@
 
 const stringSize = (text) => {
+    return text.length;
 
 }
 const replaceCharacterE = (text) => {
+   return text.replace("e", " ");
 
 }
 const concatString = (text1, text2) => {
+    return text1+text2;
 
 }
 const showChar5 = (text) => {
+    return text[4];
 
 }
 const showChar9 = (text) => {
-
+return text.substring(0,9);
 }
 const toCapitals = (text) => {
+    return text.toUpperCase();
 
 }
 const toLowerCase = (text) => {
+    return text.toLowerCase();
 
 }
 const removeSpaces = (text) => {
-
+return text.trim();
 }
 const IsString = (text) => {
+   
+    return typeof text == 'string';
+    
 
 }
 
 const getExtension = (text) => {
+    return text.split('.').pop();
 
 }
 const countSpaces = (text) => {
+    return text.split(" ").length - 1
 
 }
 const InverseString = (text) => {
-
+    return text.split("").reverse().join("");
 }
 
 const power = (x, y) => {
-
+    return Math.pow(x,y);
 }
 const absoluteValue = (num) => {
-
+    return Math.abs(num);
 }
 const absoluteValueArray = (array) => {
+    for (var i=0; i < array.length;i++){
+        array[i]= Math.abs(array[i]);
+    }
+    return array;
 
 }
 const circleSurface = (radius) => {
-
+    return Math.round(Math.PI*Math.pow(radius,2));
 }
 const hypothenuse = (ab, ac) => {
+    var hypothenuse=Math.pow(ab,2) + Math.pow(ac,2);
+    return Math.sqrt(hypothenuse);
 
 }
 const BMI = (weight, height) => {
+    
+    return parseFloat((weight/(height*height)).toFixed(2));
 
 }
 
 const createLanguagesArray = () => {
+    return [ 'Html', 'CSS', 'Java', 'PHP' ];
 
 }
 
 const createNumbersArray = () => {
-
+return [ 0, 1, 2, 3, 4, 5 ];
 }
 
 const replaceElement = (languages) => {
-
+languages[2]="Javascript";
+return languages;
 }
 
 const addElement = (languages) => {
+languages.push('Ruby','Python');
 
+return languages;
 }
 
 const addNumberElement = (numbers) => {
-
+numbers.unshift(-2,-1);
+return numbers;
 }
 
 const removeFirst = (languages) => {
-
+    languages.shift();
+    return languages;
 }
 
 const removeLast = (languages) => {
-
+languages.pop();
+return languages;
 }
 
 const convertStrToArr = (social_arr) => {
+   return  social_arr.split(',');
+    
 
 }
 
 const convertArrToStr = (languages) => {
-
+return languages.toString();
 }
 
 const sortArr = (social_arr) => {
-
+ return social_arr.sort();
 }
 
 const invertArr = (social_arr) => {
+    return social_arr.reverse();
 
 }
